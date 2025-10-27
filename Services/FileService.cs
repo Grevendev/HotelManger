005096e3169584e4748885f5b33ba17159c0b45c
Grevendev <edvin.lindborg@hotmail.se>
2025-10-27 19:43:06 +0100

    Added code and structur in FileService.cs, using both json and .txt format

---
+++ Services/FileService.cs
@@ -0,0 +1,44 @@
+using System.Text.Json;
+
+namespace Hotel;
+
+public static class FileService
+{
+  private static string rommsFile = "Data/rooms.json";
+  private static string usersFile = "Data/users.txt";
+  public static List<Room> LoadRooms()
+  {
+    if (!File.Exists(roomsFile))
+    {
+      var defaultRooms = new List<Room>();
+      for (int i = 1; i <= 10; i++)
+        defaultRooms.Add(new Room(i));
+
+      SaveRooms(defaultRooms);
+      return defaultRooms;
+    }
+    var json = File.ReadAllText(roomsFile);
+    var data = JsonSerializer.Deserialize<List<Room>>(json);
+    return data ?? new List<Room>();
+  }
+  public static void SaveRooms(List<Room> rooms)
+  {
+    var json = JsonSerializer.Deserialize(rooms, new JsonSerializerOptions { WriteIndented = true });
+    File.WriteAllText(roomsFile, json);
+  }
+  public static List<User> LoadUser()
+  {
+    var users = new List<User>();
+    if (!File.Exists(usersFile)) return users;
+
+    foreach (var line in File.ReadAllLines(usersFile))
+    {
+      var parts = line.Split(',');
+      if (parts.Length == 2)
+      {
+        users.Add(new User(parts[0].Trim(), parts[1].Trim()))
+      }
+    }
+    return users;
+  }
+}
